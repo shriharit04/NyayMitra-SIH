ef2cccbe06df4faf3f21312d42654ddf0dc2723d
Shrihari T <shriharit2004@gmail.com>
2024-09-08 00:42:46 +0530

    Added Chatbot folder, added routes, connected route with app.py and setup controller. Need to test it

---
+++ api/Chatbot/chatbotRoutes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, jsonify, request
+from chatbot import loading, generate
+
+# Define the Blueprint
+chatbot_bp = Blueprint('chatbot_bp', __name__)
+
+# Initialize the RAG chain
+rag_chain = loading()
+
+@chatbot_bp.route('/')
+def home():
+    return "Welcome to the Chatbot Page!"
+
+@chatbot_bp.route('/test_prompt', methods=['GET'])
+def prompt():
+    user_prompt = request.args.get('prompt')
+    result = {"data": f"Prompt entered: {user_prompt}"}
+    return jsonify(result)
+
+@chatbot_bp.route('/initial_load', methods=['GET'])
+def initial_load():
+    # Initialize the RAG chain (if not already initialized)
+    global rag_chain
+    rag_chain = loading()
+    result = {"data": "Initialization complete"}
+    return jsonify(result)
+
+@chatbot_bp.route('/prompt', methods=['POST'])
+def handle_query():
+    data = request.json
+    query = data.get('query', '')
+    response = generate(rag_chain, query)
+    return jsonify({"response": response}), 200
